60d9550ad5b849e4be0ec3ddf6a5604d60ad1bc8
FreddyMH <FreddyMH@DESKTOP-ICJSG9Q>
2024-02-13 21:13:19 -0600

    Agregar archivos de proyecto.

---
+++ Par Impar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_Impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (Par(num))
+            {
+                Console.WriteLine($"{num} es un número par.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"{num} es un número impar.");
+                Console.ReadKey();
+            }
+        }
+
+        static bool Par(int numero)
+        {
+            return numero % 2 == 0;
+        }
+
+    }
+}
